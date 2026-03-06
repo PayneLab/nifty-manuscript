ac6433bd6f4fc497a8f227274fe25ef724d78876
Alyssa Nitz <71338312+nitz1bug@users.noreply.github.com>
2026-03-06 11:27:27 -0700

    update table and add summary statistics prints

--- Figures_and_Tables/generate_Table_1.R
+++ Figures_and_Tables/generate_Table_1.R
@@ -11,8 +11,19 @@ table <- data %>%
   group_by(Dataset, `Data Type`, `Cell Types`, `Samples per Class`) %>%
   summarize("Validation Accuracy" = mean(`Validation Accuracy`)) %>%
   unite("Test", `Data Type`, `Samples per Class`, sep = "_") %>%
-  pivot_wider(names_from = Test, values_from = `Validation Accuracy`)
+  pivot_wider(names_from = Test, values_from = `Validation Accuracy`) %>%
+  mutate(difference_50 = Imputed_50 - Unimputed_50) %>%
+  mutate(difference_100 = Imputed_100 - Unimputed_100)
 
 write_tsv(table, "Table1.tsv")
+
+# print out the median differences reported in the manuscript
+table %>%
+  pull(difference_50) %>%
+  median()
+
+table %>%
+  pull(difference_100) %>%
+  median()
 
 
