011c00141c4955a6928ec67b301676b13f6c93fe
Alyssa Nitz <71338312+nitz1bug@users.noreply.github.com>
2026-04-08 09:41:06 -0600

    Add code for Saddic etal

--- Testing/No_Imputation/incomplete_protein_analysis.py
+++ Testing/No_Imputation/incomplete_protein_analysis.py
@@ -7,27 +7,16 @@ script_path = os.path.abspath(__file__)
 script_directory = os.path.dirname(script_path)
 
 # file paths
-output_paths = [os.path.join(script_directory, "Test_Ai_Imputed", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Ai_Unimputed", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Furtwaengler_Imputed_HSCxEarlyEryth", "NIFty_Output"), 
+output_paths = [os.path.join(script_directory, "Test_Ai_Unimputed", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Furtwaengler_Unimputed_HSCxEarlyEryth", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Furtwaengler_Imputed_HSCxEMP", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Furtwaengler_Unimputed_HSCxEMP", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Khan_Imputed", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Khan_Unimputed", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Leduc_Imputed", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Leduc_Unimputed", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Montalvo_Imputed", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Montalvo_Unimputed", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Petrosius_Imputed", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Petrosius_Unimputed", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Saddic_Imputed_WT", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Saddic_Unimputed_WT", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Saddic_Imputed_MFN", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Saddic_Unimputed_MFN", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Saddic_Imputed_SMC", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Saddic_Unimputed_SMC", "NIFty_Output"), 
-                os.path.join(script_directory, "Test_Saddic_Imputed_Fibro", "NIFty_Output"), 
                 os.path.join(script_directory, "Test_Saddic_Unimputed_Fibro", "NIFty_Output")]
 
 
@@ -36,24 +25,29 @@ data_missingness = []
 cell_types = []
 num_samples_per_class = []
 test_num = []
-val_accuracy = []
-val_precision = []
-val_recall = []
+num_total_proteins = []
+num_complete_proteins = []
+num_complete_pairs = []
 
 # loop through directories in each of the above locations and read the "model_information.txt" file
-for output_path in output_paths:
+for output_path in test_paths:
     print(f"Currently analyzing: {output_path}")
 
     for dirpath, dirnames, filenames in os.walk(output_path):
         for subdir in dirnames:
+            if "Split50" not in subdir and "Split100" not in subdir:
+                continue
             print(f"\t- {subdir}")
             
-            model_info_path = os.path.join(dirpath, subdir, "model_information.txt")
+            features_path = os.path.join(dirpath, subdir, "selected_features.tsv")
+            fs_input_path = os.path.join(output_path, "FS_Datasets", f"{subdir}_FS_Quant.tsv")
+            train_test_input_path = os.path.join(output_path, "Train_Test_Datasets", f"{subdir}_Train_Test_Quant.tsv")
+            validate_input_path = os.path.join(output_path, "Validate_Datasets", f"{subdir}_Validate_Quant.tsv")
 
             test_info = subdir.split("_")
             dataset.append(test_info[0])
             data_missingness.append(test_info[1])
-            if test_info[0] == "Furtwaengler":
+            if test_info[0] == "Furtwaengler" or test_info[0] == "Saddic":
                 cell_types.append(test_info[2])
                 num_samples_per_class.append(int(test_info[3].replace("Split", "")))
                 test_num.append(int(test_info[4].replace("Test", "")))
@@ -62,21 +56,34 @@ for output_path in output_paths:
                 num_samples_per_class.append(int(test_info[2].replace("Split", "")))
                 test_num.append(int(test_info[3].replace("Test", "")))
 
-            with open(model_info_path) as in_file:
-                for line in in_file:
-                    if line.startswith("Accuracy:") or line.startswith("Precision:") or line.startswith("Recall:"):
-                        line = line.strip().split(": ")
+            features = pd.read_csv(features_path, sep='\t')
+            fs_quant = pd.read_csv(fs_input_path, sep='\t').drop('sample_id', axis=1)
+            total_proteins = len(fs_quant.columns)
+            num_total_proteins.append(total_proteins)
+            fs_quant = fs_quant.dropna(axis=1)
+            train_test_quant = pd.read_csv(train_test_input_path, sep='\t').dropna(axis=1).drop('sample_id', axis=1)
+            validate_quant = pd.read_csv(validate_input_path, sep='\t').dropna(axis=1).drop('sample_id', axis=1)
 
-                        if line[0] == "Accuracy":
-                            val_accuracy.append(float(line[1]))
-                        elif line[0] == "Precision":
-                            val_precision.append(float(line[1]))
-                        elif line[0] == "Recall":
-                            val_recall.append(float(line[1]))
+            fs_proteins = set(fs_quant.columns.tolist())
+            train_test_proteins = set(train_test_quant.columns.tolist())
+            validate_proteins = set(validate_quant.columns.tolist())
 
-        break
+            complete_proteins = fs_proteins & train_test_proteins & validate_proteins
 
-final_output_path = os.path.join(script_directory, "combined_results.tsv")
+            complete_pairs = 0
+            protein1 = features['Protein1'].tolist()
+            protein2 = features['Protein2'].tolist()
+
+            for prot1, prot2 in zip(protein1, protein2):
+                if prot1 in complete_proteins and prot2 in complete_proteins:
+                    complete_pairs += 1
+
+            num_complete_proteins.append(len(complete_proteins))
+            num_complete_pairs.append(complete_pairs)
+
+            
+
+final_output_path = os.path.join(script_directory, "incomplete_protein_analysis_results.tsv")
 print(f"Saving final output to: {final_output_path}")
 final_output = pd.DataFrame({
     'Dataset': dataset, 
@@ -84,9 +91,9 @@ final_output = pd.DataFrame({
     'Cell Types': cell_types, 
     'Samples per Class': num_samples_per_class, 
     'Test': test_num, 
-    'Validation Accuracy': val_accuracy, 
-    'Validation Precision': val_precision, 
-    'Validation Recall': val_recall
+    'Number Proteins': num_total_proteins,
+    'Number Complete Proteins': num_complete_proteins, 
+    'Number Complete Pairs': num_complete_pairs
 })
 
 final_output.to_csv(final_output_path, sep='\t', index=False)
